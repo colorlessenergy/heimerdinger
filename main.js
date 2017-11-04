ee48ba5baafe360c52cd0b814cb18cdd68612d92
colorlessenergy <brianmunozdev@gmail.com>
2017-11-04 07:43:19 -0700

    mobile for home

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
 let containerMenu = document.querySelector(".container-menu");
 let menuRes = document.querySelector(".menu-res");
+let down = document.querySelector("#down");
+let gallery = document.querySelector(".gallery");
+let galleryDiv;
+if (gallery) {
+  galleryDiv = gallery.querySelectorAll("div");
 
+  down.addEventListener("click", function () {
+    let scroll = window.setInterval(function () {
+      if (window.scrollY < gallery.offsetTop - 50) {
+        window.scrollBy(0, 5);
+    } else if (window.scrollY > gallery.offsetTop - 50) {
+      window.clearInterval(scroll)
+    }
+    }, 10)
+  });
+}
+
+let once = false;
 
 containerMenu.addEventListener("click", function (e) {
   containerMenu.classList.toggle("change");
   menuRes.classList.toggle("toggle-nav");
+});
+
+
+
+window.setInterval(function () {
+  if (window.scrollY > 378 && !once) {
+    console.log("called", window.scrollY);
+    galleryDiv.forEach(function (current) {
+      current.classList.toggle("showPic");
+    });
+    once = true;
+  }
 })
